9b41eb9a7c7b746afb2d3e4d988502f8941fc250
Vinicius Oliveira <vinilimadeoliveira@hotmail.com>
2022-05-23 08:59:22 -0300

    Primeiro commit

---
+++ bdLocadora.sql
@@ -0,0 +1,204 @@
+﻿CREATE DATABASE	bdLocadora
+GO
+USE bdLocadora
+
+CREATE TABLE tbEstrela(
+	id							INTEGER			NOT NULL
+	,	nome 					VARCHAR(40)		NOT NULL
+	PRIMARY KEY (id)
+);
+CREATE TABLE tbCliente(
+	numCadastro					INTEGER			NOT NULL
+	,	nome					VARCHAR(70)		NOT NULL
+	,	logradouro				VARCHAR(150)	NOT NULL
+	,	num						INTEGER			NOT NULL	CHECK(num > -1)
+	,	cep						CHAR(8)						CHECK(LEN(cep) = 8)
+	PRIMARY KEY (numCadastro)
+);
+CREATE TABLE tbFilme(
+	id							INTEGER			NOT NULL
+	,	titulo					VARCHAR(40)		NOT NULL
+	,	ano						INTEGER						CHECK(ANO < 2022)
+	PRIMARY KEY (id)
+);
+CREATE TABLE tbDvd(
+	num							INTEGER			NOT NULL
+	,	dataFabricacao			DATE			NOT NULL	CHECK(dataFabricacao < GETDATE())
+	,	filmeID					INTEGER			NOT NULL
+	PRIMARY KEY (num)
+	FOREIGN KEY (filmeId)		REFERENCES tbFilme(id)
+);
+CREATE TABLE tbFilmeEstrela(
+	filmeId						INTEGER			NOT NULL
+	,	estrelaId				INTEGER			NOT NULL
+	PRIMARY KEY (filmeId,estrelaId)
+	FOREIGN KEY (filmeId)		REFERENCES tbFilme(id)
+	,	FOREIGN KEY (estrelaId) REFERENCES tbEstrela(id)
+);
+CREATE TABLE tbLocacao(
+	dvdNum						INTEGER			NOT NULL
+	,	clienteNumCadastro		INTEGER			NOT NULL
+	,	dataLocacao				DATE			NOT NULL	DEFAULT(GETDATE())
+	,	dataDevolucao			DATE			NOT NULL
+	,	valor					DECIMAL(7,2)	NOT NULL	CHECK(valor > -1)
+	PRIMARY KEY (dvdNum,clienteNumCadastro,dataLocacao)
+	FOREIGN KEY (dvdNum)						REFERENCES tbDvd(num)
+	,	FOREIGN KEY (clienteNumCadastro)		REFERENCES tbCliente(numCadastro)
+	,	CONSTRAINT dataLocacaoMaiorDevolucao				CHECK(dataDevolucao	> dataLocacao) 
+);
+--========================ALTERAÇÕES=========================
+ALTER TABLE tbEstrela
+ADD nomeReal					VARCHAR(50)
+
+ALTER TABLE tbFilme
+ALTER COLUMN titulo				VARCHAR(80)		NOT NULL
+--========================INSERTS============================
+INSERT INTO tbFilme(id,titulo,ano)
+	VALUES
+		(1001,'Whiplash',2015)
+		,(1002,'Birdman',2015)
+		,(1003,'Interestelar',2014)
+		,(1004,'A Culpa é das estrelas',2014)
+		,(1005,'Alexandre e o Dia Terrível, Horrível, Espantoso e Horroroso',2014)
+		,(1006,'Sing',2016)
+
+INSERT INTO tbEstrela(id,nome,nomeReal)
+	VALUES 
+		(9901,'Michael Keaton','Michael John Douglas')
+		,(9902,'Emma Stone','Emily Jean Stone')
+		,(9903,'Miles Teller',NULL)
+		,(9904,'Steve Carell','Steven John Carell')
+		,(9905,'Jennifer Garner','Jennifer Anne Garner')
+
+INSERT INTO tbFilmeEstrela(filmeId,estrelaId)
+	VALUES
+		(1002  ,9901)
+		,(1002 ,9902)
+		,(1001 ,9903)
+		,(1005 ,9904)
+		,(1005 ,9905)
+
+INSERT INTO tbDvd(num,	dataFabricacao, filmeID)
+	VALUES
+		(10001,'2020-12-02',1001)
+		,(10002,'2019-10-18',1002)
+		,(10003,'2020-04-03',1003)
+		,(10004,'2020-12-02',1001)
+		,(10005,'2019-10-18',1004)
+		,(10006,'2020-04-03',1002)
+		,(10007,'2020-12-02',1005)
+		,(10008,'2019-10-18',1002)
+		,(10009,'2020-04-03',1003)
+
+INSERT INTO tbCliente(numCadastro,nome,logradouro,num,cep)
+	VALUES
+		(5501 ,'Matilde Luz','Rua Síria',150,'03086040')
+		,(5502 ,'Carlos Carreiro','Rua Bartolomeu Aires',1250,'04419110')	
+		,(5503 ,'Daniel Ramalho','Rua Itajutiba',169,NULL)
+		,(5504 ,'Roberta Bento','Rua Jayme Von Rosenburg',36,NULL)
+		,(5505 ,'Rosa Cerqueira','Rua Arnaldo Simões Pinto',235,'02917110')
+
+INSERT INTO tbLocacao(dvdNum,clienteNumCadastro,dataLocacao,dataDevolucao,valor)
+	VALUES
+		(10001,5502,'2021-02-18','2021-02-21',3.50)
+		,(10009,5502,'2021-02-18','2021-02-21',3.50)
+		,(10002,5503,'2021-02-18','2021-02-19',3.50)
+		,(10002,5505,'2021-02-20','2021-02-23',3.00)
+		,(10004,5505,'2021-02-20','2021-02-23',3.00)
+		,(10005,5505,'2021-02-20','2021-02-23',3.00)
+		,(10001,5501,'2021-02-24','2021-02-26',3.50)
+		,(10008,5501,'2021-02-24','2021-02-26',3.50)
+--==============================OPERAÇÕES COM DADOS===========================
+UPDATE tbCliente
+SET cep = '08411150'
+WHERE numCadastro =  5503
+
+UPDATE tbCliente
+SET cep = '02918190'
+WHERE numCadastro =  5504
+--==
+UPDATE tbLocacao
+SET valor = 3.25
+WHERE dataLocacao = '2021-02-18' AND clienteNumCadastro = 5502
+
+UPDATE tbLocacao
+SET valor = 3.10
+WHERE dataLocacao = '2021-02-24' AND clienteNumCadastro = 5501
+
+--==
+UPDATE tbDvd
+SET dataFabricacao = '2019-07-14'
+WHERE num =  10005
+--==
+UPDATE tbEstrela
+set nomeReal = 'Miles Alexander Teller'
+WHERE id = 9903
+--==
+DELETE FROM tbFilme
+WHERE id = 1006
+
+--============================= CONSULTAS ===================================
+/*
+1) Consultar, num_cadastro do cliente, nome do cliente, titulo do filme, data_fabricação
+do dvd, valor da locação, dos dvds que tem a maior data de fabricação dentre todos os
+cadastrados.
+*/
+SELECT TOP 5
+	cli.numCadastro
+	,	cli.nome
+	,	film.titulo
+	,	dvd.dataFabricacao
+	,	loc.valor
+FROM tbCliente AS cli
+	INNER JOIN tbLocacao AS loc
+		ON loc.clienteNumCadastro = cli.numCadastro
+	INNER JOIN tbDvd as dvd
+		ON dvd.num = loc.dvdNum
+	INNER JOIN tbFilme as film
+		ON film.id = dvd.filmeID
+ORDER BY dvd.dataFabricacao DESC
+		
+/*
+2) Consultar, num_cadastro do cliente, nome do cliente, data de locação
+(Formato DD/MM/AAAA) e a quantidade de DVD ́s alugados por cliente (Chamar essa
+coluna de qtd), por data de locação
+*/
+SELECT 
+	cli.numCadastro
+	,	cli.nome
+	,	CONVERT(CHAR(10),loc.dataLocacao,103) AS 'Data da Locação'
+	,	COUNT(loc.dvdNum) AS qtd
+FROM tbCliente AS cli
+	INNER JOIN tbLocacao AS loc
+		ON loc.clienteNumCadastro = cli.numCadastro
+GROUP BY loc.dataLocacao,cli.numCadastro,cli.nome
+ORDER BY loc.dataLocacao
+/*
+3) Consultar, num_cadastro do cliente, nome do cliente, data de locação
+(Formato DD/MM/AAAA) e o valor total de todos os dvd's alugados (Chamar essa
+coluna de valor_total), por data de locação
+*/
+SELECT 
+	cli.numCadastro
+	,	cli.nome
+	,	CONVERT(CHAR(10),loc.dataLocacao,103) AS 'Data da Locação'
+	,	SUM(loc.valor) AS 'Valor total'
+FROM tbCliente AS cli
+	INNER JOIN tbLocacao AS loc
+		ON loc.clienteNumCadastro = cli.numCadastro
+GROUP BY loc.dataLocacao,cli.numCadastro,cli.nome
+/*
+4) Consultar, num_cadastro do cliente, nome do cliente, Endereço
+concatenado de logradouro e numero como Endereco, data de locação (Formato
+DD/MM/AAAA) dos clientes que alugaram mais de 2 filmes simultaneamente
+*/
+SELECT 
+	cli.numCadastro
+	,	cli.nome
+	,	cli.logradouro+ ', ' + CAST(cli.num AS CHAR(7)) AS 'Endereco'
+	,	CONVERT(CHAR(10),loc.dataLocacao,103) AS 'Data de Locação'
+FROM tbCliente AS cli
+	INNER JOIN tbLocacao AS loc
+		ON loc.clienteNumCadastro = cli.numCadastro
+GROUP BY cli.numCadastro,cli.nome,loc.dataLocacao,cli.logradouro,cli.num
+HAVING COUNT(loc.dvdNum) > 2
---
+++ bdProjetos.sql
@@ -0,0 +1,116 @@
+CREATE DATABASE bdProjetos
+GO
+USE bdProjetos
+CREATE TABLE tbProjects(
+	id						INTEGER			IDENTITY(10001,1)	NOT NULL
+	,	projectName			VARCHAR(45)							NOT NULL
+	,	projectDescription	VARCHAR(45)							
+	,	projectDate			DATE								NOT NULL	CHECK(projectDate > '01/09/2014')
+	PRIMARY KEY (id)
+);
+CREATE TABLE tbUsers(
+	id						INTEGER			IDENTITY(1,1)		NOT NULL
+	,	firstName			VARCHAR(45)							NOT NULL
+	,	username			VARCHAR(45)		UNIQUE				NOT NULL
+	,	passwd				VARCHAR(45)							NOT	NULL	DEFAULT('123mudar')
+	,	email				VARCHAR(45)							NOT	NULL
+	PRIMARY KEY (id)
+);
+CREATE TABLE tbUserHasProjects(
+	usersId					INTEGER								NOT NULL
+	,	projectsId			INTEGER								NOT NULL
+	PRIMARY KEY(usersId,projectsId)	
+	FOREIGN KEY (usersId)			REFERENCES tbUsers(id)
+	,	FOREIGN KEY (projectsId)	REFERENCES tbProjects(id)
+);
+--====================================MODIFICAÇÕES============================================
+ALTER TABLE tbUsers
+DROP CONSTRAINT UQ__tbUsers__F3DBC57207020F21
+ALTER TABLE tbUsers
+ALTER COLUMN username	VARCHAR(10) NOT NULL
+ALTER TABLE tbUsers
+ADD CONSTRAINT uniqueUserName UNIQUE (username)
+
+ALTER TABLE tbUsers
+ALTER COLUMN passwd			VARCHAR(8)		NOT	NULL
+--======================================INSERTS USUÁRIOS================================================
+INSERT INTO tbUsers(firstName,username,email)
+	VALUES
+		('Maria','Rh_maria','maria@empresa.com')
+INSERT INTO tbUsers(firstName,username,passwd,email)
+	VALUES
+		('Paulo','Ti_paulo','123@456','paulo@empresa')
+INSERT INTO tbUsers(firstName,username,email)
+	VALUES
+		('Ana','Rh_ana','ana@empresa.com')
+		,('Clara','Ti_clara','clara@empresa.com')
+INSERT INTO tbUsers(firstName,username,passwd,email)
+	VALUES
+		('Aparecido','Rh_apareci','55@!cido','aparecido@empresa.com')
+
+--=======================================INSERTS PROJETOS===============================
+INSERT INTO tbProjects(projectName, projectDescription,projectDate)
+	VALUES
+		('Re-folha','Refatoração das Folhas','05/09/2014')	
+		,('Manutenção PC''s','Manutenção PC''s','06/09/2014')
+		,('Auditoria',null,'07/09/2014')
+--=======================================INSERTS USUARIO_PROJETOS=======================
+INSERT INTO tbUserHasProjects(usersId,projectsId)
+	VALUES
+		(1,10001)
+		,(5,10001)
+		,(3,10003)
+		,(4,10002)
+		,(2,10002)
+--=======================================ALTERAÇÃO DADOS================================
+UPDATE tbProjects
+SET projectDate = '12/09/2014'
+WHERE id = 10002
+
+UPDATE tbUsers
+SET username = 'Rh_cido'
+WHERE firstName = 'Aparecido'
+
+UPDATE tbUsers
+SET passwd = '888@*'
+WHERE username = 'Rh_maria' AND passwd = '123mudar'
+
+DELETE FROM tbUserHasProjects
+WHERE usersId = 2 AND projectsId = 10002
+
+--===================
+INSERT INTO tbUsers (firstName,username,passwd,email)
+	VALUES 
+		('Joao', 'Ti_joao', '123mudar', 'joao@empresa.com')
+
+--==
+INSERT INTO tbProjects(projectName,projectDescription,projectDate)
+	VALUES
+		('Atualização de Sistemas', 'Modificação de Sistemas Operacionais nos PC''s', '12/09/2014')
+
+--========================== CONSULTAS ===============================
+/*
+1) Quantos projetos não tem usuários associados a ele. A coluna deve chamar
+qty_projects_no_users
+*/
+SELECT
+	COUNT(prj.id) AS qty_projects_no_users
+FROM tbProjects AS prj
+	LEFT OUTER JOIN tbUserHasProjects AS usrp
+		ON prj.id = usrp.projectsId
+WHERE usrp.usersId IS NULL
+GROUP BY id
+/*
+2) Id do projeto, nome do projeto, qty_users_project (quantidade de usuários por
+projeto) em ordem alfabética crescente pelo nome do projeto
+*/
+SELECT
+	prj.id
+	,	prj.projectName AS 'Nome do Projeto'
+	,	count(usrp.usersId) AS 'qty_users_project'
+FROM tbProjects AS prj
+	INNER JOIN tbUserHasProjects AS usrp
+		ON usrp.projectsId = prj.id
+GROUP BY prj.id,prj.projectName
+ORDER BY prj.projectName ASC
+		
